088d0a85074bdd45becd4f183db32b87758de3a9
Vitor Nagaya Wong <vnwgithub@gmail.com>
2024-10-10 16:04:52 -0300

    criei a tabela CandidatoVaga no phpmyadmin e criei as classes CandidatoVaga e Vagas! Agora o relacionamento entre candidato e vagas está correto!

--- src/entities/CandidatoVaga.java
+++ src/entities/CandidatoVaga.java
@@ -2,5 +2,30 @@
 package entities;
 
 public class CandidatoVaga {
+    private Candidato candidato;
+    private Vagas vaga;    
     
+    public CandidatoVaga(){        
+    }
+
+    public CandidatoVaga(Candidato candidato, Vagas vaga) {
+        this.candidato = candidato;
+        this.vaga = vaga;
+    }
+
+    public Candidato getCandidato() {
+        return candidato;
+    }
+
+    public void setCandidato(Candidato candidato) {
+        this.candidato = candidato;
+    }
+
+    public Vagas getVaga() {
+        return vaga;
+    }
+
+    public void setVaga(Vagas vaga) {
+        this.vaga = vaga;
+    }   
 }
--- src/entities/Vagas.java
+++ src/entities/Vagas.java
@@ -1,13 +1,52 @@
-
 package entities;
 
 public class Vagas {
+
     private int idVaga;
     private String descricao;
     private String requisitos;
     private double salario;
-    
-    public Vagas(){        
+
+    public Vagas() {
+    }
+
+    public Vagas(int idVaga, String descricao, String requisitos, double salario) {
+        this.idVaga = idVaga;
+        this.descricao = descricao;
+        this.requisitos = requisitos;
+        this.salario = salario;
+    }
+
+    public int getIdVaga() {
+        return idVaga;
+    }
+
+    public void setIdVaga(int idVaga) {
+        this.idVaga = idVaga;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getRequisitos() {
+        return requisitos;
+    }
+
+    public void setRequisitos(String requisitos) {
+        this.requisitos = requisitos;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
     }
     
     
